78caf34bd2031e53630cc25a239bfed8c7bb7517
Nikita Shtengauer <redfancoestar@gmail.com>
2022-01-26 01:03:39 +0300

    Add files via upload

--- utils.js
+++ utils.js
@@ -7,3 +7,31 @@ function scrollToTop(){
 		window.scrollTo(0, 0);
 	}, 20);
 }
+
+function isInViewport(el) {
+    var top = el.offsetTop;
+	var left = el.offsetLeft;
+	var width = el.offsetWidth;
+	var height = el.offsetHeight;
+
+	while(el.offsetParent) {
+		el = el.offsetParent;
+		top += el.offsetTop;
+		left += el.offsetLeft;
+	}
+
+	return (
+		top < (window.pageYOffset + window.innerHeight) &&
+		left < (window.pageXOffset + window.innerWidth) &&
+		(top + height) > window.pageYOffset &&
+		(left + width) > window.pageXOffset
+	);
+}
+
+function doesFileExist(urlToFile) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('HEAD', urlToFile, false);
+    xhr.send(null);
+     
+    return xhr.status !== 404;
+}
